9519b892223aaa112ae4d55d7dd3239ac587afc1
DylanSuarez <2000sajo@gmail.com>
2023-02-16 23:23:30 -0500

    initial setup react

---
+++ client/tailwind.config.cjs
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        blue: '#1DA1F2',
+        black: '#14171A',
+        'dark-gray': '#657786',
+        'light-gray': '#AAB8C2',
+        'ex-gray': '#E1E8ED',
+        'exe-gray': '#F5F8FA',
+      },
+    },
+  },
+  plugins: [],
+}
