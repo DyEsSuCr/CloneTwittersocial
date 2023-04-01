d49d9ba15009c680a33a318f35979ec4b82365a8
DylanSuarez <2000sajo@gmail.com>
2023-04-01 12:59:27 -0500

    recomendations

--- client/src/components/Notices/Recomendations.jsx
+++ client/src/components/Notices/Recomendations.jsx
@@ -1,9 +1,21 @@
-import { Profiles } from '../Profile/Profiles'
+import { useContext } from 'react'
+import { UserContext, AuthUserContext } from '../../context/index'
+import { Link } from 'react-router-dom'
 
 export const Recomendations = () => {
+  const { data } = useContext(UserContext)
+  const { authUser } = useContext(AuthUserContext)
+
   return (
     <div>
-      <Profiles />
+      {data?.filter(user => user.username !== authUser?.username).map((user) => (
+        <div key={user.id}>
+          <p>{user.username}</p>
+          <Link to={`/${user.username}`} className='text-red-600'>
+            {user.username}
+          </Link>
+        </div>
+      ))}
     </div>
   )
 }
